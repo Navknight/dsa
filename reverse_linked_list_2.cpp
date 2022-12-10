06ac49631a40a170bf8d10e8d869c185f9510d08
Abhinav <abhi.gupta8802@gmail.com>
2022-12-11 03:47:50 +0530

    more

---
+++ reverse_linked_list_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(NULL) {}
+    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *reverseBetween(ListNode *head, int left, int right)
+{
+    if(head == NULL || head->next == NULL || left == high)
+        return head;
+    else{
+        
+    }
+}
+
+int main()
+{
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    int left = 2;
+    int right = 4;
+    ListNode *result = reverseBetween(head, left, right);
+    while (result != NULL)
+    {
+        cout << result->val << " ";
+        result = result->next;
+    }
+    return 0;
+}
