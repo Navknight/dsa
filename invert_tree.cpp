183c0f786eca6717bb7aeb6c87010e67769a33f9
Abhinav <abhi.gupta8802@gmail.com>
2023-01-12 11:47:03 +0530

    more

---
+++ invert_tree.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *invertTree(TreeNode *root)
+{
+    if (root == NULL)
+        return root;
+    TreeNode *temp = new TreeNode();
+    temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+    root->left = invertTree(root->left);
+    root->right = invertTree(root->right);
+    return root;
+}
+
+int main()
+{
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(2);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(3);
+    root = invertTree(root);
+    cout << root->val << endl;
+    cout << root->left->val << endl;
+    cout << root->right->val << endl;
+    cout << root->left->left->val << endl;
+    cout << root->left->right->val << endl;
+    cout << root->right->left->val << endl;
+    cout << root->right->right->val << endl;
+}
