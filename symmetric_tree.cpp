2b1a1eeeb1030889bc0673450b54f6124df3670b
Abhinav <abhi.gupta8802@gmail.com>
2023-01-12 11:41:53 +0530

    more

--- symmetric_tree.cpp
+++ symmetric_tree.cpp
@@ -11,12 +11,29 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-
+bool isMirror(TreeNode *t1, TreeNode *t2)
+{
+    if (t1 == NULL && t2 == NULL)
+        return true;
+    if (t1 == NULL || t2 == NULL)
+        return false;
+    return (t1->val == t2->val) && isMirror(t1->left, t2->right) && isMirror(t1->right, t2->left);
+}
 bool isSymmetric(TreeNode *root)
 {
-    
+    if (root == NULL)
+        return true;
+    return isMirror(root->left, root->right);
 }
 
 int main()
 {
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(2);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(3);
+    cout << isSymmetric(root) << endl;
 }
