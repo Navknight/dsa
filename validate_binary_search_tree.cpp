38445484a70c06d899b73be88479b8c2bebe1976
Abhinav <abhi.gupta8802@gmail.com>
2023-01-26 18:35:20 +0530

    more

--- validate_binary_search_tree.cpp
+++ validate_binary_search_tree.cpp
@@ -2,73 +2,28 @@
 
 using namespace std;
 
-struct TreeNode
-{
+struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(NULL), right(NULL) {};
+    TreeNode(int x): val(x), left(NULL), right(NULL) {};
 };
-
-bool isValidBST(TreeNode *root)
-{
-    if (root == NULL)
-        return true;
-    if (root->left != NULL && root->right != NULL)
-    {
-        if (root->left->val < root->val && root->right->val > root->val)
-        {
-            return isValidBST(root->left) && isValidBST(root->right);
-        }
-        else
-        {
-            return false;
-        }
-    }
-    else if (root->left != NULL)
-    {
-        if (root->left->val < root->val)
-        {
-            return isValidBST(root->left);
-        }
-        else
-        {
-            return false;
-        }
-    }
-    else if (root->right != NULL)
-    {
-        if (root->right->val > root->val)
-        {
-            return isValidBST(root->right);
-        }
-        else
-        {
-            return false;
-        }
-    }
-    else
-    {
-        return true;
-    }
+bool checkBST(TreeNode* root, TreeNode* min, TreeNode* max){
+    if(!root) return true;
+    if((min && root->val <= min->val) || (max && root->val >= max->val)) return false;
+    return checkBST(root->right, root, max) && checkBST(root->left, min, root);
 }
 
-int main()
-{
-    TreeNode *root = new TreeNode(5);
+bool isValidBST(TreeNode* root) {
+    return checkBST(root, NULL, NULL);
+}
+
+int main(){
+    TreeNode* root = new TreeNode(2);
     root->left = new TreeNode(1);
-    root->right = new TreeNode(4);
-    root->right->left = new TreeNode(3);
-    root->right->right = new TreeNode(6);
-    // print binary tree
-    cout << root->val << endl;
-    cout << root->left->val << endl;
-    cout << root->right->val << endl;
-    cout << root->right->left->val << endl;
-    cout << root->right->right->val << endl;
-    cout << endl;
-    // check if binary tree is valid
+    root->right = new TreeNode(3);
+    
     cout << isValidBST(root) << endl;
+    return 0;
 }
