4df62543b248b9b2320e3a3a7297eb0bac64004b
Abhinav <abhi.gupta8802@gmail.com>
2023-01-29 11:18:08 +0530

    more

--- two-sum-IV.cpp
+++ two-sum-IV.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-bool 
+
+bool findTarget(TreeNode* root, int k) {
+    
+} 
+
+
+int main(){
+    TreeNode* root = new TreeNode(5);
+    root->left = new TreeNode(3);
+    root->right = new TreeNode(6);
+    root->left->left = new TreeNode(2);
+    root->left->right = new TreeNode(4);
+    root->right->right = new TreeNode(7);
+    int k = 9;
+    cout << findTarget(root, k) << endl;
+    return 0;
+}
