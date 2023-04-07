3bf71211b33c10d1580de33e6725b36de72333f0
navknight <abhi.gupta8802@gmail.com>
2023-04-07 10:25:39 +0530

    more

--- LeetCode/next_greater_element_3.cpp
+++ LeetCode/next_greater_element_3.cpp
@@ -4,12 +4,24 @@ using namespace std;
 
 int nextGreaterElement(int n)
 {
-    
+    string s = to_string(n);
+    int i = s.size() - 2;
+    while (i >= 0 && s[i] >= s[i + 1])
+        i--;
+    if (i < 0)
+        return -1;
+    int j = s.size() - 1;
+    while (j >= 0 && s[j] <= s[i])
+        j--;
+    swap(s[i], s[j]);
+    reverse(s.begin() + i + 1, s.end());
+    long long res = stoll(s);
+    return res > INT_MAX ? -1 : res;
 }
 
 int main()
 {
-    int n = 12;
+    int n = 21;
     cout << nextGreaterElement(n) << endl;
     return 0;
 }
