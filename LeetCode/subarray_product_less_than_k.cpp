d2e062370f9d7e0a7a2c5bb31de37de028277777
Abhinav <abhi.gupta8802@gmail.com>
2023-04-09 16:29:10 +0530

    more

--- LeetCode/subarray_product_less_than_k.cpp
+++ LeetCode/subarray_product_less_than_k.cpp
@@ -4,13 +4,31 @@ using namespace std;
 
 int numSubarrayProductLessThanK(vector<int> &nums, int k)
 {
-    
+    int mul = 1;
+    int ans = 0;
+    if (k <= 1)
+        return ans;
+    int left = 0, right = 0;
+    while (right < nums.size())
+    {
+        mul *= nums[right];
+
+        while (mul >= k)
+        {
+            mul /= nums[left];
+            left++;
+        }
+
+        ans += right - left + 1;
+        right++;
+    }
+    return ans;
 }
 
 int main()
 {
-    vector<int> nums = {10, 5, 2, 6};
-    int k = 100;
+    vector<int> nums = {10, 9, 10, 4, 3, 8, 3, 3, 6, 2, 10, 10, 9, 3};
+    int k = 19;
     cout << numSubarrayProductLessThanK(nums, k) << endl;
     return 0;
 }
