9da584913e095cccb2ee8e4648e699bcd396be19
Abhinav <abhi.gupta8802@gmail.com>
2023-05-13 18:42:22 +0530

    more

---
+++ CodeForces/A_Bit.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int x = 0;
+
+    while(n--){
+        string op;
+        cin>>op;
+
+        if(op[1] == '+') x++;
+        if(op[1] == '-') x--;
+    }
+
+    cout<<x;
+}
