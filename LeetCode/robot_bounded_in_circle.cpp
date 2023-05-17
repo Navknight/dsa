95c2fe1afaf677a253c9cb535daf5ca09bc47681
Abhinav <abhi.gupta8802@gmail.com>
2023-05-17 22:23:41 +0530

    more

--- LeetCode/robot_bounded_in_circle.cpp
+++ LeetCode/robot_bounded_in_circle.cpp
@@ -4,7 +4,32 @@ using namespace std;
 
 bool isRobotBounded(string instructions)
 {
-    
+    int x = 0, y= 0, dir = 0;
+    for(char i : instructions){
+        if(i == 'G'){
+            if(dir == 0)
+                y++;
+            else if(dir == 1)
+                x++;
+            else if(dir == 2)
+                y--;
+            else x--;
+        }
+        if(i == 'L'){
+            dir--;
+            if(dir < 0)
+                dir = 3;
+        }
+        if(i == 'R'){
+            dir++;
+            dir = dir%4;
+        }
+    }
+    if(x == 0 && y == 0)
+        return 1;
+    if(dir > 0)
+        return 1;
+    else return 0;
 }
 
 int main()
