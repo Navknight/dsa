d692658c9b5f60b2519e6c2dae71353892b42137
Abhinav <abhi.gupta8802@gmail.com>
2023-05-23 22:59:22 +0530

    more

---
+++ LeetCode/Delete_and_earn.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++>
+
+using namespace std;
+
+int deleteAndEarn(vector<int> &nums)
+{
+    
+}
+
+int main()
+{
+    vector<int> nums = {3, 4, 2};
+    cout << deleteAndEarn(nums) << endl;
+    return 0;
+}
--- LeetCode/House_robber.cpp
+++ LeetCode/House_robber.cpp
@@ -2,14 +2,30 @@
 
 using namespace std;
 
-int rob(vector<int> &nums)
-{
+int helper(vector<int>& nums, int index, vector<int>& memo) {
+    if (index >= nums.size())
+        return 0;
 
+    if (memo[index] != -1)
+        return memo[index];
+
+    int robCurrent = nums[index] + helper(nums, index + 2, memo);
+    int skipCurrent = helper(nums, index + 1, memo);
+    int maxAmount = max(robCurrent, skipCurrent);
+
+    memo[index] = maxAmount;
+    return maxAmount;
+}
+
+int rob(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> memo(n, -1);
+    return helper(nums, 0, memo);
 }
 
 int main()
 {
-    vector<int> nums = {1, 2, 3, 1};
+    vector<int> nums = {2,7,9,3,1};
     cout << rob(nums) << endl;
     return 0;
 }
