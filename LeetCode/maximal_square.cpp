c3a47370958420bd00f7bf7ea958200502933b2c
Abhinav <abhi.gupta8802@gmail.com>
2023-06-03 17:57:09 +0530

    more

---
+++ LeetCode/maximal_square.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int maximalSquare(vector<vector<char>> &matrix)
+{
+    
+}
+
+int main()
+{
+    vector<vector<char>> matrix = {{'1'}};
+    cout << maximalSquare(matrix) << endl;
+    return 0;
+}
