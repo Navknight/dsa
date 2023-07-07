60fd7992a183921dcbd3846ee752b0f726cc4124
Abhinav <abhi.gupta8802@gmail.com>
2023-07-07 23:34:19 +0530

    more

--- CodeForces/A_String_Task.cpp
+++ CodeForces/A_String_Task.cpp
@@ -12,7 +12,7 @@ int main()
     for (int i = 0; i < count; i++)
     {
         in[i] = tolower(in[i]);
-        if (in[i] == 'a' || in[i] == 'e' || in[i] == 'i' || in[i] == 'o' || in[i] == 'u')
+        if (in[i] == 'a' || in[i] == 'e' || in[i] == 'i' || in[i] == 'o' || in[i] == 'u' || in[i] == 'y')
             continue;
         else{
             out += '.';
