b1a9b72a52482a8290aa1af53a268b8287707af6
Abhinav <abhi.gupta8802@gmail.com>
2023-08-05 12:59:59 +0530

    more

--- DP-Basics/matrix_chain_multiplication.cpp
+++ DP-Basics/matrix_chain_multiplication.cpp
@@ -1,26 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int matrixMultiplication(int N, int arr[])
+// recusrion
+int mcm(int i, int j, int arr[], vector<vector<int>> &t)
 {
-    
+    if (t[i][j] != -1)
+        return t[i][j];
+
+    int mini = INT32_MAX;
+
+    if (i >= j)
+        return 0;
+    else
+    {
+        for (int k = i; k < j; k++)
+        {
+            int temp = mcm(i, k, arr, t) + mcm(k + 1, j, arr, t) + arr[i - 1] * arr[k] * arr[j];
+            mini = min(temp, mini);
+        }
+    }
+    return t[i][j] = mini;
 }
 
+int matrixMultiplication(int n, int arr[])
+{
+    vector<vector<int>> t(n, vector<int>(n, -1));
+    return mcm(1, n - 1, arr, t);
+}
 
 int main()
 {
+    ifstream fin;
+    fin.open("input.txt", ios::in);
+    ofstream fout;
+    fout.open("output.txt", ios::out);
+
     int t;
-    cin >> t;
+    fin >> t;
     while (t--)
     {
         int N;
-        cin >> N;
+        fin >> N;
         int arr[N];
         for (int i = 0; i < N; i++)
-            cin >> arr[i];
+            fin >> arr[i];
 
-        Solution ob;
-        cout << matrixMultiplication(N, arr) << endl;
+        fout << matrixMultiplication(N, arr) << endl;
     }
     return 0;
 }
