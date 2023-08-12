0df3947b79fa1f5e9683feb91ad116f07c0b0e3f
Abhinav <abhi.gupta8802@gmail.com>
2023-08-12 14:58:59 +0530

    more

--- LeetCode/number_of_longest_increasing_subsequences.cpp
+++ LeetCode/number_of_longest_increasing_subsequences.cpp
@@ -5,23 +5,42 @@ using namespace std;
 int findNumberOfLIS(vector<int> &nums)
 {
     int n = nums.size();
-    vector<int> ans (n);
+    vector<int> t(n, 0);
+    vector<int> len(n, 0);
+    vector<int> counter(n, 0);
+    int max_length = 0, ans = 0;
 
-    int maxi = INT32_MIN;
-    for(int i = n-1; i >= 0; i--){
-        int temp = 1;
-        for(int j = n-1; j > i; j--){
-            if(nums[i] < nums[j])
-                temp = max(temp, temp+ans[j]);
+    for(int i = n-1; i>=0; i--){
+        int length = 1, count = 1;
+        for(int j = i+1; j < n; j++){
+            if(nums[j] > nums[i]){
+                if(len[j] + 1 > length){
+                    length = len[j] + 1;
+                    count = counter[j];
+                }
+                else if(len[j] + 1 == length){
+                    count += counter[j];
+                }
+            }
         }
-        ans[i] = temp;
-        maxi = max(temp, maxi);
+        len[i] = length;
+        counter[i] = count;
+
+        if(length > max_length){
+            ans = count;
+            max_length = length;
+        }
+        else if(length == max_length){
+            ans += count;
+        }
     }
+
+    return ans;
 }
 
 int main()
 {
-    vector<int> nums = {1, 3, 5, 4, 7};
+    vector<int> nums = {1, 3, 2, 4};
     cout << findNumberOfLIS(nums) << endl;
     return 0;
 }
