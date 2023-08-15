27d9d3c9ac1ca850860edb466a7b4f611e621104
Abhinav <abhi.gupta8802@gmail.com>
2023-08-15 15:26:31 +0530

    more

--- DP-Basics/palindrome_partitioning.cpp
+++ DP-Basics/palindrome_partitioning.cpp
@@ -2,20 +2,39 @@
 
 using namespace std;
 
-
-
-vector<vector<string>> partition(string s)
+bool isPalindrome(string str, int start, int end)
 {
+    for (int i = start; i < (end - start + 1) / 2; i++)
+        if (str[i] != str[end - i])
+            return false;
+    return true;
+}
+
+int help(string str, int i, int j, vector<vector<int>> &t)
+{
+    if (i >= j || isPalindrome(str, i, j))
+        return t[i][j] = 0;
+    if (t[i][j] != -1)
+        return t[i][j];
+    int mini = INT32_MAX;
+    for (int k = i; k < j; k++)
+    {
+        mini = min(help(str, i, k, t) + help(str, k + 1, j, t) + 1, mini);
+    }
+
+    return t[i][j] = mini;
+}
+
+int palindromicPartition(string str)
+{
+    int n = str.size();
+    vector<vector<int>> t(n + 1, vector<int>(n + 1, -1));
+    return help(str, 0, str.size() - 1, t);
 }
 
 int main()
 {
-    vector<vector<string>> p = partition("aab");
-    for (auto i : p)
-    {
-        for (auto j : i)
-            cout << j << " ";
-        cout << endl;
-    }
+    int p = palindromicPartition("ababbbabbababa");
+    cout << p << endl;
     return 0;
 }
--- LeetCode/Best_time_stock_with_cooldown.cpp
+++ LeetCode/Best_time_stock_with_cooldown.cpp
@@ -2,27 +2,43 @@
 
 using namespace std;
 
-int help(vector<int> &prices, int i, int prev, int profit)
-{
-    if (i >= prices.size())
-        return profit;
-    else
-    {
-        if (prev == -1)
-            return max(help(prices, i + 1, prices[i], profit), help(prices, i + 1, prev, profit));
-        else
-        {
-            if (prices[i] > prev)
-                return max(help(prices, i + 2, -1, profit + prices[i] - prev), help(prices, i + 1, prev, profit));
-            else
-                return help(prices, i + 1, prev, profit);
-        }
-    }
-}
+// int help(vector<int> &prices, int i, int prev, int profit)
+// {
+//     if (i >= prices.size())
+//         return profit;
+//     else
+//     {
+//         if (prev == -1)
+//             return max(help(prices, i + 1, prices[i], profit), help(prices, i + 1, prev, profit));
+//         else
+//         {
+//             if (prices[i] > prev)
+//                 return max(help(prices, i + 2, -1, profit + prices[i] - prev), help(prices, i + 1, prev, profit));
+//             else
+//                 return help(prices, i + 1, prev, profit);
+//         }
+//     }
+// }
+
+// int maxProfit(vector<int> &prices)
+// {
+//     return help(prices, 0, -1, 0);
+// }
 
 int maxProfit(vector<int> &prices)
 {
-    return help(prices, 0, -1, 0);
+    int n = prices.size();
+    vector<vector<int>> t(n+1, vector<int> (2));
+
+    //0 is for holding stock
+    //1 is for buying
+
+    t[0][0] = 0;
+    t[0][1] = -prices[0];
+
+    for(int i  = 1; i < n+1; i++){
+        
+    }
 }
 
 int main()
